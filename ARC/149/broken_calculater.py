328480924ac32b30c61c1d2226a0bc89f818ba54
Taiki <hmyrwjj@yahoo.co.jp>
2023-09-22 21:19:37 +0900

    a

--- ARC/149/broken_calculater.py
+++ ARC/149/broken_calculater.py
@@ -4,11 +4,12 @@ n = len(str(a))
 ans = float("inf")
 
 
-for i in range(n):  # i桁目まではaと同じ値にする
-    for j in range(10):  # i+1桁目をいい感じのやつにする。初めて異なる数を使う場所。何でここが特別かよくわからん、
-        for k in range(10):  # 繰り返す数字。
+for i in range(n):
+    for j in range(10):
+        for k in range(10):
             num = str(a)[:i] + str(j) + str(k) * (n - i - 1)
             num = int(num)
             if 1 <= len(set(str(num))) <= border:
                 ans = min(ans, abs(a - int(num)))
+
 print(ans)
