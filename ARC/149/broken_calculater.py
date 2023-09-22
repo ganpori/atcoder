eff2633e8864f2a27c3e11cde07f464fc2079de9
Taiki <hmyrwjj@yahoo.co.jp>
2023-09-22 21:17:29 +0900

    tuika

--- ARC/149/broken_calculater.py
+++ ARC/149/broken_calculater.py
@@ -1,11 +1,13 @@
-a, k = list(map(int, input().split()))
+a, border = list(map(int, input().split()))
 n = len(str(a))
 
 ans = float("inf")
+
+
 for i in range(n):  # i桁目まではaと同じ値にする
-    for q in range(10):  # i+1桁目をいい感じのやつにする。初めて異なる数を使う場所。何でここが特別かよくわからん、
-        for r in range(10):  # 繰り返す数字。
-            num = n[:i] + str(q) + str(r) * (n - i - 1)
-            if 1 <= len(set(str(num))) <= k:
+    for j in range(10):  # i+1桁目をいい感じのやつにする。初めて異なる数を使う場所。何でここが特別かよくわからん、
+        for k in range(10):  # 繰り返す数字。
+            num = n[:i] + str(j) + str(k) * (n - i - 1)
+            if 1 <= len(set(str(num))) <= border:
                 ans = min(ans, abs(a - int(num)))
 print(ans)
