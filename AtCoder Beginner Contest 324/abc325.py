147700c9d425316c38c67e821824ac2b14f321fd
Taiki <hmyrwjj@yahoo.co.jp>
2023-10-21 21:30:02 +0900

    bといた

--- AtCoder Beginner Contest 324/abc325.py
+++ AtCoder Beginner Contest 324/abc325.py
@@ -1,8 +1,29 @@
+import collections
+
+
 def A():
     s, t = input().split()
     print(f"{s} san")
     return
 
 
+def B():
+    n = int(input())
+    dict_num = {i: 0 for i in range(24)}
+
+    for _ in range(n):
+        w, x = map(int, input().split())
+        for start_time_i in range(24):
+            local_start_time = start_time_i + x
+            if local_start_time >= 24:
+                local_start_time -= 24
+            if 9 <= local_start_time <= 17:
+                dict_num[start_time_i] = dict_num[start_time_i] + w
+
+    print(max(dict_num.values()))
+    # print(dict_num)
+    return
+
+
 if __name__ == "__main__":
-    A()
+    B()
