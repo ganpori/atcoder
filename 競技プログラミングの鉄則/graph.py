f81360e08541d38e5a98a3144ace09849b0a2c50
Taiki <hmyrwjj@yahoo.co.jp>
2023-10-28 20:57:51 +0900

    深さ優先探索実装。間違えてるけど

--- 競技プログラミングの鉄則/graph.py
+++ 競技プログラミングの鉄則/graph.py
@@ -19,5 +19,33 @@ def a61():
     return
 
 
+def dfs(pos, adjancy_list, list_visited):
+    list_visited[pos] = True
+    for node_from_pos in adjancy_list[pos]:
+        if list_visited[node_from_pos] == False:
+            dfs(pos=node_from_pos, adjancy_list=adjancy_list, list_visited=list_visited)
+    return
+
+
+def a62():
+    n, m = map(int, input().split())
+    list_edges = [list(map(int, input().split())) for _ in range(m)]
+    list_visited = [False] * n
+    adjacency_list = [[] for _ in range(n)]
+    for a, b in list_edges:
+        adjacency_list[b - 1].append(a - 1)
+        adjacency_list[a - 1].append(b - 1)
+
+    dfs(pos=0, adjancy_list=adjacency_list, list_visited=list_visited)
+
+    is_connected = all(list_visited)
+    if is_connected:
+        print("The graph is connected.")
+    else:
+        print("The graph is not connected.")
+
+    return
+
+
 if __name__ == "__main__":
-    a61()
+    a62()
