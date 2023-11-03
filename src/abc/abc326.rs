96f8f3c0ea881ac690d039387614ea1c300ab8ed
Taiki <hmyrwjj@yahoo.co.jp>
2023-11-04 03:36:17 +0900

    しゃくとり法でabc326のcといた

--- src/abc/abc326.rs
+++ src/abc/abc326.rs
@@ -1,3 +1,4 @@
+// https://atcoder.jp/contests/abc326
 use proconio::input;
 
 pub fn a() {
@@ -9,7 +10,35 @@ pub fn b() {
 }
 
 pub fn c() {
-    input! {n:usize,}
+    input! {
+        n:usize,
+        m:usize,
+        mut a:[usize;n]
+    }
+
+    a.sort();
+
+    let mut right = vec![0; n];
+    let mut num_present_max = 0;
+
+    for (i, ai) in a.iter().enumerate() {
+        if (i != 0) {
+            right[i] = std::cmp::max(right[i - 1], i);
+        }
+        let j_add = right[i];
+        for (j, aj) in a[right[i]..].iter().enumerate() {
+            let j_in_a = j + j_add;
+            // println!("j_in_a:{}", j_in_a);
+            if (*ai + m > *aj) {
+                num_present_max = std::cmp::max(num_present_max, j_in_a - i + 1);
+                right[i] = j_in_a;
+            } else {
+                break;
+            }
+        }
+    }
+    // println!("{:?}", a);
+    println!("{}", num_present_max);
 }
 
 pub fn d() {
