e6da013295d5420bca003b1a0e232da0b70c3cf2
Taiki <hmyrwjj@yahoo.co.jp>
2023-11-09 15:58:14 +0900

    a62実装

--- src/tessoku/graph.rs
+++ src/tessoku/graph.rs
@@ -23,3 +23,37 @@ pub fn a61() {
         println!("{}: {{{}}}", i + 1, s);
     }
 }
+
+pub fn a62() {
+    input! {
+        n:usize,
+        m:usize,
+        vec_edge:[[usize;2];m]
+    }
+    let mut adjacency_list = vec![Vec::<usize>::new(); n];
+    for edge in vec_edge.iter() {
+        adjacency_list[edge[0] - 1].push(edge[1] - 1);
+        adjacency_list[edge[1] - 1].push(edge[0] - 1);
+    }
+
+    // println!("{:?}", adjacency_list);
+    let mut vec_visited = vec![false; n];
+    dfs(0, &mut vec_visited, &adjacency_list);
+
+    if vec_visited.iter().all(|x| *x) {
+        println!("The graph is connected.");
+    } else {
+        println!("The graph is not connected.");
+    }
+}
+
+fn dfs(pos: usize, vec_visited: &mut Vec<bool>, adjacency_list: &Vec<Vec<usize>>) {
+    vec_visited[pos] = true;
+    for adjacency_node in adjacency_list[pos].iter() {
+        if vec_visited[*adjacency_node] {
+            continue;
+        } else {
+            dfs(*adjacency_node, vec_visited, adjacency_list) //ここをmut とか&つけなくていいのなぜかわからん
+        }
+    }
+}
