5f7c4b8a65f53bfbbeb7a7362b87aa3a86cdfb96
Taiki <hmyrwjj@yahoo.co.jp>
2023-11-17 02:08:15 +0900

    abc320のc解いた // 順序を与えて条件を単純化する。 // どちらも推せる可能性があっても順序を与えて場合分けするとシンプル // 解くというよりはシミュレーションをする。その仕方を工夫する。

--- src/abc/ab320.rs
+++ src/abc/ab320.rs
@@ -36,3 +36,56 @@ fn is_palindrome(a: &[char]) -> bool {
     }
     true
 }
+
+// 順序を与えて条件を単純化する。
+// どちらも推せる可能性があっても順序を与えて場合分けするとシンプル
+// 解くというよりはシミュレーションをする。その仕方を工夫する。
+pub fn c() {
+    input! {
+        m:usize,
+        s: [Chars;3],
+    }
+
+    let mut vec_target_value = Vec::<usize>::new();
+    for i in 0..=9 as usize {
+        let i_char = std::char::from_digit(i as u32, 10).unwrap();
+        if s[0].contains(&i_char) && s[1].contains(&i_char) && s[2].contains(&i_char) {
+            vec_target_value.push(i);
+        }
+    }
+    if vec_target_value.len() == 0 {
+        println!("-1");
+        return;
+    }
+    let mut ans = 3 * m + 1;
+    for target_value in &vec_target_value {
+        let target_char = std::char::from_digit(*target_value as u32, 10).unwrap();
+        // println!("target_char: {}", target_char);
+        for perm in (0..3 as usize).permutations(3) {
+            let result = solve(&s, target_char, perm);
+            // println!("result:{}", result);
+            ans = std::cmp::min(result, ans);
+            // println!("ans:{}", ans);
+        }
+    }
+    println!("{}", ans);
+}
+
+fn solve(s: &Vec<Vec<char>>, target_char: char, perm: Vec<usize>) -> usize {
+    // println!("{:?}", perm);
+    let m = s[0].len();
+    for i in 0..m {
+        if s[perm[0]][i] == target_char {
+            for j in i + 1..i + 1 + m {
+                if s[perm[1]][j % m] == target_char {
+                    for k in j + 1..j + 1 + m {
+                        if s[perm[2]][k % m] == target_char {
+                            return k;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    3 * m + 1
+}
