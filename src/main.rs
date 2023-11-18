4afbd2007cb2f83555fd296e2ab229ee58909bae
Taiki <hmyrwjj@yahoo.co.jp>
2023-11-19 04:51:30 +0900

    mainを少しきれいにした

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,16 @@
 // https://qiita.com/drken/items/dc53c683d6de8aeacf5a
 // https://atcoder.jp/contests/dp
 
+// rustのコレクションまとめ
+// https://qiita.com/garkimasera/items/a6df4d1cd99bc5010a5e
+
 // mod abc;
 // mod tessoku;
 // https://atcoder.jp/contests/tessoku-book
 
 use proconio::input;
-// use proconio::marker::Chars;
+use proconio::marker::Chars;
 
 fn main() {
-    // tessoku::binary_search::a13();
-    // abc::abc323::c();
-    // c();
+    // a();
 }
