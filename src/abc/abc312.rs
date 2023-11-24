45d56e4322efad9f5207cd3df96bbd86b0ff8aa4
Taiki <hmyrwjj@yahoo.co.jp>
2023-11-24 22:29:06 +0900

    abc312のc。二分探索。範囲を広めて境界の条件を考えないようにする。
    // 境界でどうなってるかの扱いがなんでも難しい。
    // 二分探索で境界が怪しいときは辺に特殊例を場合分けせずに、アルゴリズムに任せる。
    // 具体的には探索範囲を広めにとって微妙な境界も探索のアルゴリズムで判断するようにする

--- src/abc/abc312.rs
+++ src/abc/abc312.rs
@@ -88,3 +88,57 @@ fn is_tak(h: usize, w: usize, code: &Vec<Vec<char>>) -> bool {
     }
     is_tak
 }
+
+// 境界でどうなってるかの扱いがなんでも難しい。
+// 二分探索で境界が怪しいときは辺に特殊例を場合分けせずに、アルゴリズムに任せる。
+// 具体的には探索範囲を広めにとって微妙な境界も探索のアルゴリズムで判断するようにする
+fn c() {
+    input! {
+        n:usize,
+        m:usize,
+    mut a:[usize;n], //　売り手
+    mut b:[usize;m]  // 買い手
+        }
+    a.sort();
+    b.sort();
+    // 人数の差は単調増加関数
+
+    let mut left_x = a[0] - 1; // 売り手
+    let mut right_x = b[m - 1] + 1; // 買い手
+
+    if left_x > right_x {
+        print!("{}", right_x);
+        return;
+    }
+    let mut middle_x = (left_x + right_x) / 2;
+    while left_x < right_x {
+        let mut num_urite = 0;
+        for i in 0..n {
+            if a[i] <= middle_x {
+                num_urite += 1;
+            } else {
+                break;
+            }
+        }
+        let mut num_kaite = 0;
+        for i in 0..m {
+            if b[m - i - 1] >= middle_x {
+                num_kaite += 1;
+            } else {
+                break;
+            }
+        }
+        let num_diff = num_urite - num_kaite;
+        dbg!(middle_x, left_x, right_x, num_diff);
+        if num_diff > 0 {
+            right_x = middle_x
+        } else if num_diff < 0 {
+            left_x = middle_x + 1;
+        } else if num_diff == 0 {
+            right_x = middle_x
+        }
+        middle_x = (left_x + right_x) / 2;
+    }
+
+    println!("{}", middle_x);
+}
