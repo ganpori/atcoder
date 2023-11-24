15335053932b223a7636fc0881d98cb828967e24
Taiki <hmyrwjj@yahoo.co.jp>
2023-11-24 19:54:09 +0900

    abc313のcといた。シミュレーション的だが場合をちゃんと考えなきゃ

--- src/abc/abc313.rs
+++ src/abc/abc313.rs
@@ -59,3 +59,37 @@ fn dfs(pos: usize, visited: &mut Vec<bool>, g: &Vec<Vec<usize>>) {
         }
     }
 }
+
+// 片方だけじゃなくて両方の側で最大値を判断しないといけない。
+// どうしたらそう思えるだろう->
+// ->手書き実験or経験則or怖いからそうしておくor論理で小さいほうの数と大きいほうの数が入れ替わることがあるときづく？
+// 何か理想の形の数列があって、それに近づけていく、という着想も大事。
+// https://qiita.com/hatoo@github/items/fa14ad36a1b568d14f3e
+// 整数のオーバーフロー
+// RustではDebugビルド時のみオーバーフロー、負のオーバーフローを実行時に検出します。
+// オンライン上で実行されるときはReleaseビルドで実行されるのでオーバーフローは検出されないので注意しましょう。
+// 64bitだとusizeは2^64 -1=18,446,744,073,709,551,615で19桁の整数までを表せる
+fn c() {
+    input! {
+      n:usize,
+      a:[usize;n]
+    }
+
+    let average_floor: usize = a.iter().sum::<usize>() / n;
+    let average_upper;
+    if a.iter().sum::<usize>() % n == 0 {
+        average_upper = average_floor
+    } else {
+        average_upper = average_floor + 1;
+    }
+    let mut sum_diff_lower = 0;
+    let mut sum_diff_upper = 0;
+    for i in 0..n {
+        if a[i] < average_floor {
+            sum_diff_lower += average_floor - a[i];
+        } else if a[i] >= average_upper {
+            sum_diff_upper += a[i] - average_upper;
+        }
+    }
+    println!("{}", std::cmp::max(sum_diff_lower, sum_diff_upper));
+}
