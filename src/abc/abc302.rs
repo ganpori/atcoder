b0745b95f87956e50318703bdb70047030e6edcc
Taiki Iwahori <hmyrwjj@yahoo.co.jp>
2023-12-09 03:46:49 +0900

    abc302のbといた。diffをlistじゃなくてdxとdyでやったほうがよさげ。

--- src/abc/abc302.rs
+++ src/abc/abc302.rs
@@ -10,6 +10,51 @@ fn a() {
     println!("{}", val);
 }
 
+fn b() {
+    input! {
+        h:isize,
+        w:isize,
+        s:[Chars;h],
+    }
+
+    let list_diff = [
+        [[0, 0], [0, 1], [0, 2], [0, 3], [0, 4]],
+        [[0, 0], [1, 0], [2, 0], [3, 0], [4, 0]],
+        [[0, 0], [1, 1], [2, 2], [3, 3], [4, 4]],
+        [[0, 0], [-1, 1], [-2, 2], [-3, 3], [-4, 4]],
+        [[0, 0], [0, -1], [0, -2], [0, -3], [0, -4]],
+        [[0, 0], [-1, 0], [-2, 0], [-3, 0], [-4, 0]],
+        [[0, 0], [-1, -1], [-2, -2], [-3, -3], [-4, -4]],
+        [[0, 0], [1, -1], [2, -2], [3, -3], [4, -4]],
+    ];
+
+    for i in 0..h {
+        for j in 0..w {
+            for diff in list_diff {
+                let mut is_snuke = true;
+                for (k, c) in ['s', 'n', 'u', 'k', 'e'].iter().enumerate() {
+                    let next_h = i + diff[k][0];
+                    let next_w = j + diff[k][1];
+                    if 0 <= next_h && next_h <= h - 1 && 0 <= next_w && next_w <= w - 1 {
+                        if *c == s[next_h as usize][next_w as usize] {
+                        } else {
+                            is_snuke = false;
+                        }
+                    } else {
+                        is_snuke = false;
+                    }
+                }
+                if is_snuke {
+                    for k in 0..5 {
+                        println!("{} {}", i + diff[k][0] + 1, j + diff[k][1] + 1)
+                    }
+                    return;
+                }
+            }
+        }
+    }
+}
+
 // absをつけないで片側からまず抑える。その後別のifで逆方向から抑える。
 //　引き算判定は足し算に置き換えて値の範囲が辺にならないように調整
 fn d() {
