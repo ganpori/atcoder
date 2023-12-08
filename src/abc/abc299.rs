109bab0615e4c97a289bc0b96583e3cf8ed9753a
Taiki Iwahori <hmyrwjj@yahoo.co.jp>
2023-12-08 18:54:28 +0900

    abc299のbといた

--- src/abc/abc299.rs
+++ src/abc/abc299.rs
@@ -18,3 +18,26 @@ fn a() {
 
     println!("out");
 }
+
+fn b() {
+    input! {
+        n:usize,
+        t:usize,
+        c:[usize;n],
+        r:[usize;n]
+    }
+    let mut map = HashMap::new();
+    for i in 0..n {
+        let entry = map.entry(c[i]).or_insert(Vec::new());
+        entry.push([r[i], i]);
+    }
+    if map.contains_key(&t) {
+        let a = map.get_mut(&t).unwrap();
+        a.sort();
+        println!("{}", a[a.len() - 1][1] + 1);
+    } else {
+        let a = map.get_mut(&c[0]).unwrap();
+        a.sort();
+        println!("{}", a[a.len() - 1][1] + 1);
+    }
+}
