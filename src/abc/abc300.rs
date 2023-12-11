e87a942abfe1b317817219cc03c5a5cfbade6224
Taiki Iwahori <hmyrwjj@yahoo.co.jp>
2023-12-11 19:12:09 +0900

    abc300のbといた

--- src/abc/abc300.rs
+++ src/abc/abc300.rs
@@ -11,3 +11,31 @@ fn a() {
         }
     }
 }
+
+fn b() {
+    input! {
+        h:usize,
+        w:usize,
+        a:[Chars;h],
+        b:[Chars;h]
+
+    }
+
+    for hs in 0..=h - 1 {
+        for ws in 0..=w - 1 {
+            let mut is_same = true;
+            for i in 0..h {
+                for j in 0..w {
+                    if a[(i + hs) % h][(j + ws) % w] != b[i][j] {
+                        is_same = false;
+                    }
+                }
+            }
+            if is_same {
+                print!("Yes");
+                return;
+            }
+        }
+    }
+    print!("No");
+}
