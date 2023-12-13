5d52bdb567172019e118e7ad0e273796bfc24d4a
Taiki Iwahori <hmyrwjj@yahoo.co.jp>
2023-12-14 00:48:05 +0900

    abv299のdといた。 インタラクティブにやるのが大変だった

--- src/abc/abc299.rs
+++ src/abc/abc299.rs
@@ -41,3 +41,33 @@ fn b() {
         println!("{}", a[a.len() - 1][1] + 1);
     }
 }
+// インタラクティブにやるときはproconioとstdinをいい感じにしないといけない
+// inputのところも変更必要
+fn d() {
+    use proconio::{input, marker::Chars, source::line::LineSource};
+    use std::io::{stdin, BufReader};
+    let stdin = stdin();
+    let mut source = LineSource::new(BufReader::new(stdin.lock()));
+
+    input! {
+      from &mut source,
+      n:usize,
+    }
+
+    let mut left = 0;
+    let mut right = n - 1;
+    let mut middle = (left + right) / 2;
+    while left < right {
+        println!("? {}", middle + 1);
+        input! {
+        from &mut source,
+        value:usize}
+        if value == 1 {
+            right = middle;
+        } else if value == 0 {
+            left = middle + 1;
+        }
+        middle = (left + right) / 2;
+    }
+    println!("! {}", middle);
+}
