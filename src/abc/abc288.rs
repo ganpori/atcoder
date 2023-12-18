1159864b98ed8140d609055977c785bdcecd4298
Taiki Iwahori <hmyrwjj@yahoo.co.jp>
2023-12-18 18:46:30 +0900

    abc288のbといた。sortは各要素が持つ比較手法を使って比較する。// stringは違う文字が出てきた時点でunicordのコードポイントの値で比較する。 // それは辞書順になるといって差し支えない

--- src/abc/abc288.rs
+++ src/abc/abc288.rs
@@ -7,3 +7,19 @@ fn a() {
         println!("{}", ab[i][0] + ab[i][1]);
     }
 }
+
+//sortは各要素が持つ比較手法を使って比較する
+// stringは違う文字が出てきた時点でunicordのコードポイントの値で比較する。
+// それは辞書順になるといって差し支えない
+fn b() {
+    input! {
+      n:usize,
+      k:usize,
+      s:[String;n]
+    }
+    let mut sk: Vec<&String> = s.iter().take(k).collect();
+    sk.sort();
+    for i in 0..k {
+        println!("{}", sk[i]);
+    }
+}
