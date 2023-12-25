eb89627666dba1b6a93ca0c1dd863076d21c2fbb
Taiki Iwahori <hmyrwjj@yahoo.co.jp>
2023-12-25 23:48:15 +0900

    abc266のbといた

--- src/abc/abc266.rs
+++ src/abc/abc266.rs
@@ -4,3 +4,17 @@ fn a() {
     }
     println!("{}", s[(s.len()) / 2]);
 }
+
+fn b() {
+    input! {
+      n:isize,
+
+    }
+
+    for x in 0..998244353 {
+        if (n - x) % 998244353 == 0 {
+            println!("{}", x);
+            return;
+        }
+    }
+}
