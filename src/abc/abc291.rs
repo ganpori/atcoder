7b47f9fd5e017ac9b5506de1ab7b0e7affa49edf
Taiki Iwahori <hmyrwjj@yahoo.co.jp>
2023-12-27 02:06:46 +0900

    abc291のcといた

--- src/abc/abc291.rs
+++ src/abc/abc291.rs
@@ -24,3 +24,32 @@ fn b() {
     }
     println!("{}", sum as f64 / (3.0 * n as f64));
 }
+
+fn c() {
+    input! {
+      n:usize,
+      s:Chars
+    }
+    let mut trajectory_set = HashSet::new();
+    let mut pos = [0, 0];
+    trajectory_set.insert(pos);
+    for query in s {
+        if query == 'R' {
+            pos[0] += 1;
+        } else if query == 'L' {
+            pos[0] -= 1;
+        } else if query == 'U' {
+            pos[1] += 1;
+        } else if query == 'D' {
+            pos[1] -= 1;
+        }
+
+        if trajectory_set.contains(&pos) {
+            println!("Yes");
+            return;
+        } else {
+            trajectory_set.insert(pos);
+        }
+    }
+    println!("No");
+}
