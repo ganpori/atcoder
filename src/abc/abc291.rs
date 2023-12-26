b05804850f075bba9aaba853504588afb7344944
Taiki Iwahori <hmyrwjj@yahoo.co.jp>
2023-12-26 23:40:47 +0900

    abc291のbといた

--- src/abc/abc291.rs
+++ src/abc/abc291.rs
@@ -10,3 +10,17 @@ fn a() {
         }
     }
 }
+
+fn b() {
+    input! {
+      n:usize,
+     mut  x:[usize;5*n]
+    }
+    x.sort();
+    let mut sum = 0;
+
+    for i in n..4 * n {
+        sum += x[i];
+    }
+    println!("{}", sum as f64 / (3.0 * n as f64));
+}
