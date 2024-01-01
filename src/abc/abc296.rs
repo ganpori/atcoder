02a3a358ed7bad6a8c0e6891fd90c0506a17c3e6
Taiki Iwahori <hmyrwjj@yahoo.co.jp>
2024-01-01 15:20:31 +0900

    296のbといた

--- src/abc/abc296.rs
+++ src/abc/abc296.rs
@@ -12,3 +12,18 @@ fn a() {
     }
     println!("Yes");
 }
+
+fn b() {
+    input! {
+      s:[Chars;8],
+    }
+
+    for i in 0..8 {
+        for j in 0..8 {
+            if s[i][j] == '*' {
+                println!("{}{}", ('a' as u8 + j as u8) as char, 8 - i);
+                return;
+            }
+        }
+    }
+}
