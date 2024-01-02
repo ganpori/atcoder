b464a09e14bdb471f4f5a24d72bbddb5c29f0c0d
Taiki Iwahori <hmyrwjj@yahoo.co.jp>
2024-01-02 10:07:53 +0900

    294のbといた

--- src/abc/abc294.rs
+++ src/abc/abc294.rs
@@ -11,3 +11,23 @@ fn a() {
         }
     }
 }
+
+fn b() {
+    input! {
+      h:usize,
+      w:usize,
+      a:[[usize;w];h]
+    }
+
+    for i in 0..h {
+        for j in 0..w {
+            if a[i][j] == 0 {
+                print!(".");
+            } else {
+                let c = ('A' as u8 + a[i][j] as u8 - 1) as char;
+                print!("{}", c);
+            }
+        }
+        println!("");
+    }
+}
