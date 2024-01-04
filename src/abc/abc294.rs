c29dc2157120eb2a4afd015afb6c6a6acba11e07
Taiki Iwahori <hmyrwjj@yahoo.co.jp>
2024-01-04 12:44:55 +0900

    294のcといた

--- src/abc/abc294.rs
+++ src/abc/abc294.rs
@@ -31,3 +31,36 @@ fn b() {
         println!("");
     }
 }
+
+fn c() {
+    input! {
+      n:usize,
+      m:usize,
+      a:[usize;n],
+      b:[usize;m]
+    }
+
+    let mut ab = [a.clone(), b.clone()].concat();
+    // dbg!(&ab);
+    ab.sort();
+    let set_a: HashSet<usize> = a.into_iter().collect();
+    let set_b: HashSet<usize> = b.into_iter().collect();
+
+    let mut aa = vec![];
+    let mut bb = vec![];
+    for i in 0..n + m {
+        if set_a.contains(&ab[i]) {
+            aa.push(i + 1);
+        } else {
+            bb.push(i + 1);
+        }
+    }
+
+    for i in 0..n {
+        print!("{} ", aa[i]);
+    }
+    println!("");
+    for i in 0..m {
+        print!("{} ", bb[i]);
+    }
+}
