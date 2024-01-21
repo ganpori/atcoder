4fcc8de920b6aab3ee60833179130893ec906309
Taiki Iwahori <hmyrwjj@yahoo.co.jp>
2024-01-21 22:57:00 +0900

    286abd

--- src/abc/abc286.rs
+++ src/abc/abc286.rs
@@ -1,4 +1,3 @@
-
 fn a() {
     input! {
       n:usize,
@@ -9,18 +8,31 @@ fn a() {
       a:[usize;n]
     }
 
-    for i in 0..n{
-      if p<=i+1 && i+1<=q{ 
-      print!("{} ",a[r+i-p]);
+    for i in 0..n {
+        if p <= i + 1 && i + 1 <= q {
+            print!("{} ", a[r + i - p]);
+        } else if r <= i + 1 && i + 1 <= s {
+            print!("{} ", a[p + i - r]);
+        } else {
+            print!("{} ", a[i]);
+        }
     }
-    else if r<=i+1 && i+1<=s{
-      print!("{} ",a[p+i-r]);
+}
+
+fn b() {
+    input! {
+      n:usize,
+      s:Chars
     }
-    
-    else{
-      print!("{} ",a[i]);
+
+    for i in 0..n - 1 {
+        print!("{}", s[i]);
+        if s[i] == 'n' && s[i + 1] == 'a' {
+            print!("y");
+        }
     }
-}}
+    print!("{}", s.last().unwrap());
+}
 
 fn c() {
     input! {
@@ -46,4 +58,47 @@ fn c() {
     println!("{}", min_cost);
     // 1: n,2,4,6,8,..,n-2,n
     // 2: n-1,1,3,5
+}
+
+fn d() {
+    input! {
+      n:usize,
+      x:usize,
+      ab:[[usize;2];n]
+    }
+
+    let mut num_coins = 0;
+    let mut vec_value = vec![];
+    for i in 0..n {
+        num_coins += ab[i][1];
+        for j in 0..ab[i][1] {
+            vec_value.push(ab[i][0]);
+        }
+    }
+
+    let mut dp: Vec<Vec<bool>> = vec![vec![false; 10001]; num_coins + 1];
+
+    // 0円効果を使ったとき。1枚で0円を達成できる
+    dp[0][0] = true;
+
+    // dbg!(&vec_value);
+    for i in 1..=num_coins {
+        let value = vec_value[i - 1];
+        for j in 0..=10000 {
+            if j as i32 - value as i32 >= 0 {
+                if dp[i - 1][j - value] == true {
+                    dp[i][j] = true;
+                }
+            }
+            if dp[i - 1][j] == true {
+                dp[i][j] = true;
+            }
+        }
+    }
+
+    if dp[num_coins][x] {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
